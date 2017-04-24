08acdb8a252d8e228bdfe3e3eacb2beca22e0e63
Roman Iakobchuk <r.iakobchuk@gmail.com>
2017-04-24 23:00:43 +0300

    add stateful article

--- src/Article.js
+++ src/Article.js
@@ -1,15 +1,37 @@
-import React from 'react'
+import React, {Component} from 'react'
 
-export default function Article(props) {
-    const {article} = props
-    return (
-        <section>
-            <h2>
-                {article.title}
-            </h2>
-            <div>
-                {article.text}
-            </div>
-        </section>
-    )
+export default class Article extends Component {
+/*
+    constructor() {
+        super()
+        this.state = {
+            isOpen: false
+        }
+    }
+*/
+    state = {
+        isOpen: false
+    }
+
+    render() {
+        const {article} = this.props
+        return (
+            <section>
+                <h2 onClick={this.toggleOpen}>
+                    {article.title}
+                </h2>
+                {this.getBody()}
+            </section>
+        )
+    }
+
+    getBody() {
+        return this.state.isOpen && <div>{this.props.article.text}</div>
+    }
+
+    toggleOpen = (ev) => {
+        this.setState({
+            isOpen: !this.state.isOpen
+        })
+    }
 }
