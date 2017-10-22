30fc06df8962489ab80717d8c85ccd864ac8eb7d
stud2009E <stud2009@yandex.ru>
2017-10-22 22:30:31 +0300

    add Comment to Article

--- src/Article.js
+++ src/Article.js
@@ -1,37 +1,74 @@
-import React, {Component} from 'react'
+import React, {Component} from 'react';
+import Comment from './Comment.js';
 
 export default class Article extends Component {
-/*
+
     constructor() {
-        super()
+        super();
         this.state = {
-            isOpen: false
-        }
-    }
-*/
-    state = {
-        isOpen: false
+            isOpen: false,
+            isCommOpen: false
+        };
     }
 
     render() {
-        const {article} = this.props
+        const {article} = this.props;
         return (
             <section>
                 <h2 onClick={this.toggleOpen}>
                     {article.title}
                 </h2>
                 {this.getBody()}
+                {this.getComments()}
             </section>
         )
     }
 
     getBody() {
-        return this.state.isOpen && <div>{this.props.article.text}</div>
+        return this.state.isOpen &&
+            <div>
+                {this.props.article.text}
+            </div>
+    }
+
+    getComments(){
+        const {comments} = this.props.article;
+        const aCommentItems = comments && comments.map(comment =>
+                <li key={comment.id}>
+                    <Comment comment={comment}/>
+                </li>
+            );
+
+        return (
+            <div >{
+                this.state.isOpen && <h4 onClick={this.toggleCommOpen}>
+                    {this._getCommentsHead()}
+                </h4>
+                }
+                {
+                    this.state.isCommOpen && <ul>
+                        {aCommentItems}
+                    </ul>
+                }
+            </div>
+        );
+    }
+
+    _getCommentsHead(){
+        return this.state.isCommOpen ? 'Close comments' : 'Show comments';
+    }
+
+    toggleCommOpen =(ev) =>{
+        this.setState({
+            isOpen: this.state.isOpen,
+            isCommOpen: !this.state.isCommOpen
+        });
     }
 
     toggleOpen = (ev) => {
         this.setState({
-            isOpen: !this.state.isOpen
-        })
+            isOpen: !this.state.isOpen,
+            isCommOpen: this.state.isOpen
+        });
     }
 }
